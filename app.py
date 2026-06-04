53c363d27b4d19dfb583773d00153fc9ac09b63b
Subhadip Saha <sahaprofessional111@gmail.com>
2026-06-04 19:13:04 +0530

    Smart Expense Tracker V1

---
+++ app.py
@@ -0,0 +1,127 @@
+from flask import Flask, render_template, request, redirect, Response
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///expense.db"
+
+db = SQLAlchemy(app)
+
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    amount = db.Column(db.Float)
+   
+class Income(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    source = db.Column(db.String(100))
+    amount = db.Column(db.Float)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    if request.method == "POST":
+
+        title = request.form["title"]
+        amount = request.form["amount"]
+
+        expense = Expense(
+            title=title,
+            amount=amount
+        )
+
+        db.session.add(expense)
+        db.session.commit()
+
+        return redirect("/")
+
+    expenses = Expense.query.all()
+    incomes = Income.query.all()
+
+    total_expense = sum(expense.amount for expense in expenses)
+    total_income = sum(income.amount for income in incomes)
+
+    balance = total_income - total_expense  
+    
+
+    return render_template(
+    "index.html",
+    expenses=expenses,
+    total_expense=total_expense,
+    total_income=total_income,
+    balance=balance
+)
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+
+    expense = Expense.query.get(id)
+
+    db.session.delete(expense)
+
+    db.session.commit()
+
+    return redirect("/")
+
+
+@app.route("/income", methods=["GET", "POST"])
+@app.route("/export")
+def export():
+
+    expenses = Expense.query.all()
+
+    csv_data = "Title,Amount\n"
+
+    for expense in expenses:
+        csv_data += f"{expense.title},{expense.amount}\n"
+
+    return Response(
+        csv_data,
+        mimetype="text/csv",
+        headers={
+            "Content-Disposition":
+            "attachment; filename=expenses.csv"
+        }
+    )
+def income():
+
+
+
+    if request.method == "POST":
+
+        source = request.form["source"]
+        amount = request.form["amount"]
+
+        new_income = Income(
+            source=source,
+            amount=amount
+        )
+
+        db.session.add(new_income)
+        db.session.commit()
+
+        return redirect("/income")
+
+    incomes = Income.query.all()
+
+    total_income = sum(income.amount for income in incomes)
+
+    return render_template(
+        "income.html",
+        incomes=incomes,
+        total_income=total_income
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
